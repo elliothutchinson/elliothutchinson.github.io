1c407c177852e4061cd3473f2d6582dbd6bd3763
elliothutchinson <elliot@elliothutchinson.com>
2024-03-14 23:50:25 -0400

    Update url

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
+# local
 pelican -l -r
 
+# publish
+pelican -s publishconf.py
+
+# tailwind
 npm run build
--- web/portfolio/publishconf.py
+++ web/portfolio/publishconf.py
@@ -8,8 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://elliothutchinson.github.io"
-# SITEURL = "https://elliothutchinson.com"
+SITEURL = "https://elliothutchinson.com"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
