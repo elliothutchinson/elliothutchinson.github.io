cfcb6426befd43faed9cfdf1fb3f3dac87463c5c
elliothutchinson <elliot@elliothutchinson.com>
2024-03-14 23:39:58 -0400

    Update site url

--- web/portfolio/publishconf.py
+++ web/portfolio/publishconf.py
@@ -8,7 +8,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://elliothutchinson.com"
+SITEURL = "https://elliothutchinson.github.io"
+# SITEURL = "https://elliothutchinson.com"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
